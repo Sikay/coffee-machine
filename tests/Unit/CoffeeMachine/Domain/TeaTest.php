c156e45adbe71ebd6eee8d88e9d73beff7d536f6
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-03-31 16:25:07 +0200

    Added test that use a dataProvider with valid values

--- tests/Unit/CoffeeMachine/Domain/TeaTest.php
+++ tests/Unit/CoffeeMachine/Domain/TeaTest.php
@@ -4,9 +4,27 @@ namespace GetWith\Tests\Unit\CoffeeMachine\Domain;
 
 use PHPUnit\Framework\TestCase;
 use GetWith\CoffeeMachine\Domain\Tea;
+use Iterator;
 
 class TeaTest extends TestCase
 {
+    public function validPriceProvider(): iterator
+    {
+        yield "zero point five" => [0.5];
+        yield "ten point two" => [10.2];
+        yield "three" => [3];
+        yield "zero point four" => [0.4];
+    }
+
+    /** 
+     * @test 
+     * @dataProvider validPriceProvider
+     * */
+    public function should_create_tea_when_giving_it_a_higher_or_same_price(float $money): void
+    {
+        $this->assertTrue((new Tea($money))->price() <= $money);
+    }
+
     /** @test */
     public function should_create_tea_when_giving_it_a_higher_price(): void
     {
