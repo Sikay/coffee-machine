0238b8fdfd5133d218d9567b17b543a2e985ec8b
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-03-31 23:37:47 +0200

    created DTO for more desacopling

---
+++ src/CoffeeMachine/Application/DataTransformer/DrinkDtoDataTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace GetWith\CoffeeMachine\Application\DataTransformer;
+
+use GetWith\CoffeeMachine\Domain\Drink;
+
+class DrinkDtoDataTransformer
+{
+    private $drink;
+
+    public function write(Drink $drink)
+    {
+        $this->drink = $drink;
+
+        return $this;
+    }
+
+    public function read()
+    {
+        return $this->drink->order();
+    }
+}
--- src/CoffeeMachine/Application/MakeDrink.php
+++ src/CoffeeMachine/Application/MakeDrink.php
@@ -3,17 +3,23 @@
 namespace GetWith\CoffeeMachine\Application;
 
 use GetWith\CoffeeMachine\Domain\DrinkService;
+use GetWith\CoffeeMachine\Application\DataTransformer\DrinkDtoDataTransformer;
 
 class MakeDrink
 {
     public function execute(MakeDrinkRequest $request): string {
         try {  
-            return DrinkService::makeDrink(
+            $drink = DrinkService::makeDrink(
                 $request->drink(),
                 $request->money(),
                 $request->sugar(),
                 $request->extraHot(),
-            )->order();
+            );
+
+            $drinkDTO = new DrinkDtoDataTransformer();
+            $drinkDTO->write($drink);
+            return $drinkDTO->read();
+
         } catch (\InvalidArgumentException $exception) {
             return $exception->getmessage();
         }
