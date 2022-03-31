264ce7d444908784de15ce99d25ce28415787aeb
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-03-31 15:13:47 +0200

    WIP: should_create_tea_when_giving_it_a_higher_price

---
+++ tests/Unit/CoffeeMachine/Domain/TeaTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GetWith\Tests\Unit\CoffeeMachine\Domain;
+
+use PHPUnit\Framework\TestCase;
+
+class TeaTest extends TestCase
+{
+    /** @test */
+    public function should_create_tea_when_giving_it_a_higher_price(): void
+    {
+        $higherPrice = 0.5;
+        $tea = new Tea($higherPrice);
+        $this->assertTrue($tea->price() <= $higherPrice);
+    }
+}
