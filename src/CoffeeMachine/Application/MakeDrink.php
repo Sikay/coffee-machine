fa258b437e60c33e65570895998a05bd76b0daec
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-03-31 22:15:15 +0200

    should_make_drink_with_invalid_drink

--- src/CoffeeMachine/Application/MakeDrink.php
+++ src/CoffeeMachine/Application/MakeDrink.php
@@ -7,6 +7,11 @@ use GetWith\CoffeeMachine\Domain\Drink;
 class MakeDrink
 {
     public function execute(string $drinkType, float $money, int $sugars, string $extraHot): string {
+        $isValidOrder = Drink::isValidOrder($drinkType, $money, $sugars, $extraHot);
+        if (isset($isValidOrder) && !empty($isValidOrder)) {
+            return $isValidOrder;
+        }
+
         return Drink::orderedDrinkMessage($drinkType, $extraHot, $sugars);
     }
 }
--- tests/Unit/CoffeeMachine/Application/MakeDrinkTest.php
+++ tests/Unit/CoffeeMachine/Application/MakeDrinkTest.php
@@ -12,4 +12,10 @@ class MakeDrinkTest extends TestCase
     {
         $this->assertSame((new MakeDrink())->execute('chocolate', '0.7', 1, ''), 'You have ordered a chocolate with 1 sugars (stick included)');
     }
+
+    /** @test */
+    public function should_make_drink_with_invalid_drink(): void
+    {
+        $this->assertSame((new MakeDrink())->execute('', '0.7', 1, ''), 'The drink type should be tea, coffee or chocolate.');
+    }
 }
