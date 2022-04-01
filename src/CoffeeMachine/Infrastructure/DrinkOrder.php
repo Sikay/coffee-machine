a49744e7d4eb7618e2a3840c000915852b2e0d16
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-01 14:53:25 +0200

    extracted command configure to another file

--- src/CoffeeMachine/Infrastructure/DrinkOrder.php
+++ src/CoffeeMachine/Infrastructure/DrinkOrder.php
@@ -18,37 +18,7 @@ class DrinkOrder implements CommandInterface
 
     public function configure(): array
     {
-        return [
-            'argument' => [
-                'drink-type' => [
-                    'name'          => 'drink-type',
-                    'mode'          => 'REQUIRED',
-                    'description'   => 'The type of the drink. (Tea, Coffee or Chocolate)',
-                    'default-value' => null
-                ],
-                'money' => [
-                    'name'          => 'money',
-                    'mode'          => 'REQUIRED',
-                    'description'   => 'The amount of money given by the user',
-                    'default-value' => null
-                ],
-                'sugars' => [
-                    'name'          => 'sugars',
-                    'mode'          => 'OPTIONAL',
-                    'description'   => 'The number of sugars you want. (0, 1, 2)',
-                    'default-value' => 0
-                ],
-            ],
-            'option' => [
-                'extra-hot' => [
-                    'name'          => 'extra-hot',
-                    'shortcut'      => 'e',
-                    'mode'          => 'NONE',
-                    'description'   => 'The number of sugars you want. (0, 1, 2)',
-                    'default-value' => null
-                ]
-            ]
-        ];
+        return include 'OrderConfigureCommand.php';
     }
 
     public function execute(InputInterface $input): string
